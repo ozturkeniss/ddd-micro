2ebcc38a9e70bfed38392f71182de8ccb8f00177
ozturkeniss <enisozturk75@gmail.com>
2025-10-20 02:47:10 +0300

    ✨ Add EKS module variables for DDD Microservices infrastructure
    - Introduced variables.tf to define key variables for the EKS module, including name prefix, VPC ID, subnet IDs, node groups configuration, and resource tags.
    - Structured variables to facilitate easy customization and management of the EKS environment.

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,34 @@
+# EKS Module Variables
+
+variable "name_prefix" {
+  description = "Name prefix for resources"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
+
+variable "node_groups" {
+  description = "EKS node groups configuration"
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    min_size      = number
+    max_size      = number
+    desired_size  = number
+    disk_size     = number
+  }))
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
